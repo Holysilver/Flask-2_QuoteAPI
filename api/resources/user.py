5913e7c81a32bef8e9b3a675ec954a8189379924
Peter Kharikhin <Holysilver@gmail.com>
2022-02-08 19:49:32 +0300

    Add user

---
+++ api/resources/user.py
@@ -0,0 +1,29 @@
+from api import Resource, reqparse, db
+from api.models.user import UserModel
+from api.schemas.user import user_schema, users_schema
+
+
+class UserResource(Resource):
+    def get(self, user_id=None):
+        if user_id is None:
+            user = UserModel.query.all()
+            return users_schema.dump(user), 200
+        user = UserModel.query.get(user_id)
+        if not user:
+            return {"Error": f"User with {user_id} not found"}, 400
+        return user_schema.dump(user), 200
+
+    # @auth.login_required
+    # def post(self, author_id):
+    #     ...
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', required=True)
+        parser.add_argument('password', required=True)
+        data = parser.parse_args()
+        user = UserModel(**data)
+        db.session.add(user)
+        db.session.commit()
+        return user_schema.dump(user), 201
+
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from api import api, app
 from api.resources.quote import QuoteResource
 from api.resources.author import AuthorResource
 from config import Config
+from api.resources.user import UserResource
 
 api.add_resource(QuoteResource,
                  '/authors/<int:author_id>/quotes/<int:quote_id>',
@@ -11,6 +12,9 @@ api.add_resource(QuoteResource,
 api.add_resource(AuthorResource,
                  '/authors/<int:author_id>',
                  '/authors')  # <-- requests
+api.add_resource(UserResource,
+                 '/users/<int:user_id>',    # GET
+                 '/users')  # POST
 
 if __name__ == '__main__':
     app.run(debug=Config.DEBUG, port=Config.PORT)
